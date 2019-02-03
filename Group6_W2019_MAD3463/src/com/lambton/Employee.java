6f463a00bcce4f8a54b9f7573eaa92665857c68e
Md Anwar Ali <mohammed.ayubali0007@gmail.com>
2019-02-03 13:20:07 -0500

    Emplyee Main EmployeeDetails

---
+++ Group6_W2019_MAD3463/src/com/lambton/Employee.java
@@ -0,0 +1,15 @@
+package com.lambton;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Employee {
+    public static void main(String[] args) {
+EmployeeDetails emp=new EmployeeDetails();
+emp.Details();
+emp.calBirthYear();
+    }
+}
+
+
+
+
